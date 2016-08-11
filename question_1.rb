12399b60de1be3863d9b1a3bd2710dc4b1433de4
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-08-11 22:06:24 +0200

    Answers question 1.

---
+++ question_1.rb
@@ -0,0 +1,23 @@
+class HashOperations
+  def self.count_characters(input_hash)
+    sum_of_character_counts = Hash.new(0)
+
+    input_hash.each do |input|
+      sum_of_character_counts[input] += 1
+    end
+
+    sum_of_character_counts
+  end
+end
+
+describe HashOperations do
+  it "counts the number of characters in a hash" do
+    input = ["a", "a", "b"]
+    expectation = {"a" => 2,
+                   "b" => 1}
+
+    resulting_hash = HashOperations.count_characters(input)
+
+    expect(resulting_hash).to eq(expectation)
+  end
+end
