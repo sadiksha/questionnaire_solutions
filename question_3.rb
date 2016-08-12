14a03b24fcb96e7697155e3fc707715e923d1ad3
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-08-12 10:59:06 +0200

    Makes test description of question 3 consistent.

--- question_3.rb
+++ question_3.rb
@@ -11,11 +11,11 @@ describe NumberAddition do
     @sum = NumberAddition.add(x, y)
   end
 
-  it "asserts positive result from addition of two numbers" do
+  it "returns positive result from addition of two numbers" do
     expect(@sum).to be(4)
   end
 
-  it "asserts the result to be non negative and greater than decimal number" do
+  it "returns non negative and non decimal number for addition of whole numbers" do
     expect(@sum).to be > 0
     expect(@sum).to be > 0.1
   end
