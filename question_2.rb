83f8f007795930ea7413acfeeab5ed99a3ac6f78
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-08-12 10:57:36 +0200

    Adds test for question 2.

--- question_2.rb
+++ question_2.rb
@@ -1,13 +1,28 @@
 class Foo
   def self.hello_world
-    puts __method__
+    "#{__method__}"
   end
 
   def self.hi
-    puts __method__
+    "#{__method__}"
   end
 
   def method_missing(meth,*args)
-    puts "undefined method #{meth}"
+    "undefined method #{meth}"
+  end
+end
+
+describe Foo do
+  it "returns the name of called class method" do
+    first_expectation = "hello_world"
+    expect(Foo.hello_world).to eq(first_expectation)
+
+    second_expectation = "hi"
+    expect(Foo.hi).to eq(second_expectation)
+  end
+
+  it "returns message for undefined instance method" do
+    expectation = "undefined method bar"
+    expect(Foo.new().bar).to eq(expectation)
   end
 end
